c1f037fcc895060dfe0f2ac6c176f1a06e457af9
Chintan170218 <chintantiwari7@gmail.com>
2022-06-01 11:46:30 +0100

    By Branch 1 Jaydip

--- src/main/java/project/practice/class1.java
+++ src/main/java/project/practice/class1.java
@@ -5,5 +5,6 @@ public class class1 {
     public static void main(String[] args) {
         System.out.println("Main Branch");
         System.out.println("Branch_1");
+        System.out.println("Jaydip");
     }
 }
